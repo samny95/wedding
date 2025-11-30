00cb5b201e4792d4d91ea26077856c04ca3e0ab0
Sam Son <sosson97@gmail.com>
2025-11-29 17:29:10 -0800

    q

---
+++ config.js
@@ -0,0 +1,21 @@
+export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_SHARE_IMAGE =
+  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+
+export const WEDDING_DATE = "2026년 3월 2일, 월요일 17시 30분";
+export const WEDDING_LOCATION = "엘리에나 호텔, ...";
+
+export const GROOM_NAME = "손샘";
+export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_FATHER_NAME = "○○○";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_NAME = "○○○";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+
+export const BRIDE_NAME = "이나영";
+export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_FATHER_NAME = "○○○";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_NAME = "○○○";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pathPrefix: "/wedding",  // <- repo name
+  siteMetadata: {
+    title: `weddingInvitation`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
